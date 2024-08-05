50f16bcbec5393cb377e76a885594a10c29be633
Miguelitoceo07 <167791817+Miguelitoceo07@users.noreply.github.com>
2024-08-05 13:54:16 -0300

    Update calculadora.py
    codigo da calculadora

--- calculadora.py
+++ calculadora.py
@@ -1,5 +1,15 @@
-def get_user_input():
-    num1 = float(input("Digite o primeiro número: "))
-    operation = input("Digite a operação (+, -, *, /): ")
-    num2 = float(input("Digite o segundo número: "))
-    return num1, operation, num2
+# Definindo funções para cada operação
+def soma(a, b):
+    return a + b
+
+def subtracao(a, b):
+    return a - b
+
+def multiplicacao(a, b):
+    return a * b
+
+def divisao(a, b):
+    if b != 0:
+        return a / b
+    else:
+        return "Erro: Divisão por zero!"
