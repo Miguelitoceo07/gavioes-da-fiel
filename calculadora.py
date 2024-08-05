e7b60d6cd4e78148f47db6c1f3d6902cf6578c9b
vitormanoelbarbosadapaixao <167786408+vitormanoelbarbosadapaixao@users.noreply.github.com>
2024-08-05 14:00:19 -0300

    Update calculadora.py
    codigo da calculadora

--- calculadora.py
+++ calculadora.py
@@ -13,3 +13,26 @@ def divisao(a, b):
         return a / b
     else:
         return "Erro: Divisão por zero!"
+# Função principal da calculadora
+def calculadora():
+    print("Selecione o número da operação desejada:")
+    print("1 - Soma")
+    print("2 - Subtração")
+    print("3 - Multiplicação")
+    print("4 - Divisão")
+
+    escolha = input("Digite sua escolha (1/2/3/4): ")
+
+    num1 = float(input("Digite o primeiro número: "))
+    num2 = float(input("Digite o segundo número: "))
+
+    if escolha == '1':
+        print(num1, "+", num2, "=", soma(num1, num2))
+    elif escolha == '2':
+        print(num1, "-", num2, "=", subtracao(num1, num2))
+    elif escolha == '3':
+        print(num1, "*", num2, "=", multiplicacao(num1, num2))
+    elif escolha == '4':
+        print(num1, "/", num2, "=", divisao(num1, num2))
+    else:
+        print("Opção inválida!")
