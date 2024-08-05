e9a457cf23f80738452294e8f28f33649758d4b2
Miguelitoceo07 <167791817+Miguelitoceo07@users.noreply.github.com>
2024-08-05 12:23:43 -0300

    Create calculadora.py
    codigo da calculadora

---
+++ calculadora.py
@@ -0,0 +1,5 @@
+def get_user_input():
+    num1 = float(input("Digite o primeiro número: "))
+    operation = input("Digite a operação (+, -, *, /): ")
+    num2 = float(input("Digite o segundo número: "))
+    return num1, operation, num2
